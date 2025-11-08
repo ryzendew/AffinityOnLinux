0b8c57f1080e7f0fad56ce2ca93ea22b99777a75
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2025-11-08 16:53:33 +0800

    Removed buildscript.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a build directory if it doesn't exist
-mkdir -p build
-
-# Run pyinstaller
-pyinstaller --onefile --windowed --name AffinityLinuxInstaller --icon=icons/Affinity.png AffinityScripts/AffinityLinuxInstaller.py --distpath ./build
